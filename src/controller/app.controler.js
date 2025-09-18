84eb290f7ca855ae5b2d533ce80aaa0b967cb2b0
j-wojtyniak <j-wojtyniak@wp.pl>
2025-09-18 19:34:12 +0200

    Refactor search view

--- src/controller/app.controler.js
+++ src/controller/app.controler.js
@@ -1,12 +1,11 @@
 import { getPokemon } from "../services/pokeapi.service.js";
-import * as SearchView from "../view/search.view.js";
+import SearchView from "../view/search.view.js";
 import { mapApiToPokemon } from "../model/pokemon.model.js";
 import { state } from "../model/state.js";
 
 // Handles the user's search result
 const handleSearch = async function (query) {
   const pokemon = await getPokemon(query);
-  console.log(pokemon);
   await mapApiToPokemon(pokemon);
   console.log(state);
 };
--- src/view/search.view.js
+++ src/view/search.view.js
@@ -1,20 +1,24 @@
-const inputEl = document.querySelector("#search-input");
+class SearchView {
+  _inputEl = document.querySelector("#search-input");
 
-// Returns the search input
-export function getQuery() {
-  return inputEl.value.trim().toLowerCase();
+  // Returns the search input
+  getQuery() {
+    return this._inputEl.value.trim().toLowerCase();
+  }
+
+  // Adds a query returning handler to the search input
+  onSearch(handler) {
+    this._inputEl.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        handler(this.getQuery());
+        this.clearInput();
+      }
+    });
+  }
+
+  clearInput() {
+    this._inputEl.value = "";
+  }
 }
 
-// Adds a query returning handler to the search input
-export function onSearch(handler) {
-  inputEl.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      handler(getQuery());
-      clearInput();
-    }
-  });
-}
-
-const clearInput = function () {
-  inputEl.value = "";
-};
+export default new SearchView();
